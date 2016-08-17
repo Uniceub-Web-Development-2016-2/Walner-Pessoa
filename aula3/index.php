ab89596bf80b3ffec233a8d12f81375937fbdc33
WalnerPessoa <walleu@gmail.com>
2016-08-17 14:49:35 -0300

    melhorias no codigo. criticando o protocolo e aperfeiçoando os parametros

--- aula3/index.php
+++ aula3/index.php
@@ -1,20 +1,12 @@
 <?php
-
 //var_dump($_SERVER);
 include("request.php");
-
 		$method = $_SERVER['REQUEST_METHOD']; 
 		$protocol = $_SERVER['SERVER_PROTOCOL'];
 		$ipRemote = $_SERVER['REMOTE_ADDR'];
 		$ipServidor = $_SERVER['SERVER_ADDR'];
+		$resource = $_SERVER['REQUEST_URI'];
 		//$this->resource = $resource;
 		$parameters = $_SERVER['QUERY_STRING'];
-
-
-
-$request = new Request($method, $protocol, $ipRemote, $ipServidor , $parameters);
+$request = new Request($method, $protocol, $ipRemote, $ipServidor , $resource, $parameters);
 var_dump($request);
-
-
-
-
--- aula3/request.php
+++ aula3/request.php
@@ -1,56 +1,61 @@
 <?php
-
 //var_dump($_SERVER);
-
-
-
 class Request
 {
     private $method;
     private $protocol;
     private $ipServidor;
     private $ipRemote;
-    //private $resource;
+    private $resource;
     private $parameters;
-
-    public function __construct($method, $protocol, $ipRemote, $ipServidor, $parameters)
+    public function __construct($method, $protocol, $ipRemote, $ipServidor, $resource,  $parameters)
     {
 		$this->method = $method; 
-		$this->protocol = substr($protocol,0,-4);;
+		$this->protocol = self::checkProtocol($protocol);
 		$this->ipRemote = $ipRemote;
 		$this->ipServidor = $ipServidor;
-		//$this->resource = $resource;
-
-		$this->parameters = self::toArray($parameters);
+		$this->resource = self::getResource($resource);
+		//$this->parameters = self::toArray($parameters);
+		$this->parameters = $this->toArray($parameters);
 
     }
-
-
 	public function __set($atrib, $value)
 	{
-	
 		$this->$atrib = $value;
 	}
 	public function __get($atrib)
 	{
 		return $this->$atrib;
 	}
+
+	public function getResource($resource)
+	{
+    	$resourceArray= explode("/", $resource);
+
+    	return $resourceArray[1];
+	}
+	
+	public function checkProtocol($protocol)
+	{
+		if (substr($protocol,0,4)== 'HTTP'){
+				return substr($protocol,0,-4);
+			}else{ 
+				print_r (substr($protocol,0,4)." Esse protocolo digitado não é aceito. Digitar nova requisição");
+			}
+	}
 	
     public function toArray($parametros)
     {
-		//$result = $this->protocol.'://'.$this->ip.'/'.$this->resource.'?';
-    	//$parametros=$this->parameters;
     	$paramArray= explode("&", $parametros);
 
-    	return $paramArray;
-
+    	if ($paramArray[0]!="")
+    	{   		
+            foreach($paramArray as $key=>$value)
+            {
+   	    		$eachParameters= explode("=", $paramArray[$key]);	    	
+        		$arrayParamameters[$eachParameters[0]]=$eachParameters[1];
+        	} 
+    		return $arrayParamameters;
+    	}
 	}
-
-
 }
-
-
-//$request = new Request();
-//var_dump($request);
-
-
