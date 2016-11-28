543936d3b4854b2ff630607802937140258b12a3
WalnerPessoa <root@MacBook-Pro-de-Walner.local>
2016-11-27 22:59:13 -0200

    retirando pastas e arquivos desnecessarios

--- aula7/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('request_controller.php');
-
-$controller = new RequestController();
-
-
-
-echo json_encode($controller->execute());
--- aula8/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('request_controller.php');
-
-$controller = new RequestController();
-
-
-echo json_encode($controller->execute());
