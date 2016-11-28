b4b9c47c5a2d5473bb7778042f9684fd63786dee
WalnerPessoa <root@MacBook-Pro-de-Walner.local>
2016-11-27 23:20:41 -0200

    mudandass

--- client/db_manager.php
+++ client/db_manager.php
@@ -11,10 +11,10 @@ class DBConnector extends PDO {
    
     public function __construct(){
         $this->engine = 'mysql';
-        $this->host = 'localhost:3306';
+        $this->host = 'localhost';
         $this->database = 'db_PoesiAPP';
         $this->user = 'root';
-        $this->pass = 'root';
+        $this->pass = '';
         $dns = $this->engine.':dbname='.$this->database.";host=".$this->host;
         //var_dump($dns);
         parent::__construct( $dns, $this->user, $this->pass );
